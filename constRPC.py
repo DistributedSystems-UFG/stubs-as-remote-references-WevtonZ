0972ef4841bf54fcaefa660135ca9b368edfa84c
WevtonZ <wevtonj@gmail.com>
2026-06-01 11:47:26 -0300

    funciona

--- constRPC.py
+++ constRPC.py
@@ -6,9 +6,9 @@ CREATE   = '5'
 STOP     = '6'
 
 HOSTS   = '172.31.24.65'      # IP do Servidor
-PORTS   = 5689
-PORTC1  = 5689
-PORTC2  = 5690
+PORTS   = 5679
+PORTC1  = 5679
+PORTC2  = 5679
 HOSTC1  = '172.31.23.208'      # IP do Client 1
 HOSTC2  = '172.31.24.182'      # IP do Client 2
 
